b1e9de85bf8a56fc0b61da8d0c45a073d6916a4d
RESHMA S NAIR <reshmanair182012@gmail.com>
2021-01-25 23:51:05 +0530

    checkbox component and delete function modified

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
+import Checkbox from './Checkbox';
+
  
 class TodoItems extends Component {
 
-  createTasks(item) {
-    return (<li onClick={() => this.delete(item.key)} 
-             key={item.key}>{item.text}{item.key}</li>)
- }
-
- delete(key) {
-  this.props.delete(key);
-}
   constructor(props) {
-      super(props);
-   
-      this.createTasks = this.createTasks.bind(this);
+    super(props);
+    this.createTasks = this.createTasks.bind(this);
   }
 
- 
+  createTasks(item) {    
+    return (
+      <li>
+        <Checkbox
+          label={item.text}
+          handleCheckboxChange={this.props.delete}
+          value={item.key}
+          key={item.key}
+        />
+      </li>
+    );
+ }
+
   render() {
     var todoEntries = this.props.entries;
     var listItems = todoEntries.map(this.createTasks);
  
     return (
       <ul className="theList">
-          {listItems}
+        {listItems}
       </ul>
     );
   }
